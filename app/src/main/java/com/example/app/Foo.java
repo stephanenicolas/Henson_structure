c5cf8288314bc0383a4efcadec3bd67f58f2dd6d
Stéphane Nicolas <steff.nicolas@gmail.com>
2017-11-16 09:44:57 -0500

    big clean up. Everything works

--- app/src/main/java/com/example/app/Foo.java
+++ app/src/main/java/com/example/app/Foo.java
@@ -1,4 +1,4 @@
-package com.example.module2;
+package com.example.app;
 
 import android.content.Intent;
 import android.os.Bundle;
--- module1/src/main/java/com/example/module1/Foo.java
+++ module1/src/main/java/com/example/module1/Foo.java
@@ -1,6 +1,14 @@
 package com.example.module1;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class Foo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Foo");
+    }
 }
