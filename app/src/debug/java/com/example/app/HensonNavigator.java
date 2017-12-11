dd1bd9d329b896271401c6de007a9da092631916
Stéphane Nicolas <steff.nicolas@gmail.com>
2017-12-11 14:01:08 -0800

    use variant aware communication mechanism

--- app/src/debug/java/com/example/app/HensonNavigator.java
+++ app/src/debug/java/com/example/app/HensonNavigator.java
@@ -1,4 +1,8 @@
 package com.example.app;
 import android.content.Context;
+import com.example.module1.Foo__IntentBuilder;
 public class HensonNavigator {
+public static Foo__IntentBuilder gotoFoo(Context context) {
+  return new Foo__IntentBuilder(context);
+}
 }
