fdb72c4af6543d766488f9e77917769548ba6461
Stéphane Nicolas <steff.nicolas@gmail.com>
2017-12-13 16:29:26 -0800

    works with the new design

--- app/src/debug/java/com/example/app/HensonNavigator.java
+++ app/src/debug/java/com/example/app/HensonNavigator.java
@@ -2,7 +2,8 @@ package com.example.app;
 import android.content.Context;
 import com.example.module1.Foo__IntentBuilder;
 public class HensonNavigator {
-public static Foo__IntentBuilder gotoFoo(Context context) {
-  return new Foo__IntentBuilder(context);
+  public static Foo__IntentBuilder gotoFoo(Context context) {
+    return new Foo__IntentBuilder(context);
+  }
 }
-}
+
